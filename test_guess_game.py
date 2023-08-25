1691bb798b315112abccae1b8f4e3d053b17e2f9
raeshao <143127543+raeshao@users.noreply.github.com>
2023-08-25 20:39:52 +0930

    Update test_guess_game.py

--- test_guess_game.py
+++ test_guess_game.py
@@ -14,6 +14,35 @@ class TestGuessGame(unittest.TestCase):
         hint = game.generate_hint('1324')
         self.assertEqual(hint, 'circle x x circle ')
 
+    def test_player_wins(self):
+        game = GuessGame()
+        game.secret_number = '1234'
+        result = game.evaluate_guess('1234')
+        self.assertIn("Congratulations", result)
+        self.assertEqual(game.attempts, 1)
+
+    def test_player_guesses_wrong_then_wins(self):
+        game = GuessGame()
+        game.secret_number = '1234'
+        result1 = game.evaluate_guess('5678')
+        result2 = game.evaluate_guess('1234')
+        self.assertIn("Hint:", result1)
+        self.assertIn("Congratulations", result2)
+        self.assertEqual(game.attempts, 2)
+
+    def test_invalid_guess(self):
+        game = GuessGame()
+        result = game.evaluate_guess('abcd')
+        self.assertIn("Invalid guess", result)
+
+    def test_quit_game(self):
+        game = GuessGame()
+        result = game.evaluate_guess('quit')
+        self.assertEqual(result, "Quitting the game.")
+
+    # 添加更多测试用例...
+
 if __name__ == '__main__':
     unittest.main()
+
 

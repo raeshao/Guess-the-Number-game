440677f87e27fdc6ad8e15a379eaddd8e91a4ffd
raeshao <143127543+raeshao@users.noreply.github.com>
2023-08-25 20:34:07 +0930

    Update GuessGame.py

--- GuessGame.py
+++ GuessGame.py
@@ -1,44 +1,47 @@
 import random
 
 class GuessGame:
-    def __init__(self, target_number=None):
-        self.target_number = target_number if target_number is not None else str(random.randint(1000, 9999))
+    def __init__(self):
+        self.secret_number = self.generate_secret_number()
         self.attempts = 0
-        self.is_game_over = False
 
-    def check_guess(self, guess):
-        if self.is_game_over:
-            return "The game is already over. Start a new game."
+    def generate_secret_number(self):
+        return str(random.randint(1000, 9999))
 
+    def get_guess(self):
+        return input("Enter your guess: ")
+
+    def evaluate_guess(self, guess):
         self.attempts += 1
-        if guess == self.target_number:
-            self.is_game_over = True
-            return f"Congratulations! You guessed the number in {self.attempts} attempts."
+        if guess == self.secret_number:
+            return "Congratulations! You guessed the number in {} attempts.".format(self.attempts)
+        else:
+            hint = self.generate_hint(guess)
+            return "Hint: {}".format(hint)
 
-        hints = []
+    def generate_hint(self, guess):
+        hint = ''
         for i in range(4):
-            if guess[i] == self.target_number[i]:
-                hints.append('circle')
-            elif guess[i] in self.target_number:
-                hints.append('x')
+            if guess[i] == self.secret_number[i]:
+                hint += 'circle '
+            elif guess[i] in self.secret_number:
+                hint += 'x '
             else:
-                hints.append(' ')
-        
-        return "Hints: " + ', '.join(hints)
+                hint += '- '
+        return hint
 
-    def quit_game(self):
-        self.is_game_over = True
-        return f"The game has ended. You made {self.attempts} attempts."
-
-    def play_game(self):
-        while not self.is_game_over:
-            guess = input("Enter your guess (or 'quit' to exit): ")
+    def play(self):
+        print("Welcome to the Number Guessing Game!")
+        while True:
+            guess = self.get_guess()
             if guess.lower() == 'quit':
-                return self.quit_game()
-            result = self.check_guess(guess)
+                print("Quitting the game.")
+                break
+            result = self.evaluate_guess(guess)
             print(result)
+            if guess == self.secret_number:
+                break
 
-# ... 其他测试用例 ...
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    game = GuessGame()
+    game.play()

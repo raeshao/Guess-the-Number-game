d7e5eb555e02f92c8e047fefdebd57443dcee047
raeshao <143127543+raeshao@users.noreply.github.com>
2023-08-25 22:39:10 +0930

    Update test_guess_game.py

--- test_guess_game.py
+++ test_guess_game.py
@@ -1,28 +1,35 @@
+"""Test guess number game"""
 import unittest
-from GuessGame import GuessGame
+from GuessGame import GuessNumberGame
+
 
 class TestGuessGame(unittest.TestCase):
+    """Class reprenting Test Guess Game"""
     def test_generate_secret_number(self):
-        game = GuessGame()
+        """Function: test generate secret number"""
+        game = GuessNumberGame()
         secret_number = game.secret_number
         self.assertTrue(secret_number.isdigit())
         self.assertEqual(len(secret_number), 4)
 
     def test_generate_hint(self):
-        game = GuessGame()
+        """Function: test generate hint"""
+        game = GuessNumberGame()
         game.secret_number = '1234'
         hint = game.generate_hint('1324')
         self.assertEqual(hint, 'circle x x circle ')
 
     def test_player_wins(self):
-        game = GuessGame()
+        """Function: test player wins"""
+        game = GuessNumberGame()
         game.secret_number = '1234'
         result = game.evaluate_guess('1234')
         self.assertIn("Congratulations", result)
         self.assertEqual(game.attempts, 1)
 
     def test_player_guesses_wrong_then_wins(self):
-        game = GuessGame()
+        """Function: test player guesses wrong then wins"""
+        game = GuessNumberGame()
         game.secret_number = '1234'
         result1 = game.evaluate_guess('5678')
         result2 = game.evaluate_guess('1234')
@@ -33,6 +40,5 @@ class TestGuessGame(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
 
 

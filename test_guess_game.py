ce2ccd5237e6bf4c4736faf312787aed5d43b86f
raeshao <143127543+raeshao@users.noreply.github.com>
2023-08-25 19:38:46 +0930

    Update test_guess_game.py

--- test_guess_game.py
+++ test_guess_game.py
@@ -4,16 +4,16 @@ from GuessGame import GuessGame
 class TestGuessGame(unittest.TestCase):
     def test_random_number_generation(self):
         game = GuessGame()
-        self.assertTrue(1000 <= game.target_number <= 9999)
+        self.assertTrue(1000 <= int(game.target_number) <= 9999)
 
     def test_correct_guess(self):
-        game = GuessGame(target_number=1234)
-        result = game.check_guess(1234)
+        game = GuessGame(target_number='1234')
+        result = game.check_guess('1234')
         self.assertEqual(result, ("Congratulations! You guessed the number.", 1))
 
     def test_incorrect_guess(self):
-        game = GuessGame(target_number=5678)
-        result = game.check_guess(1234)
+        game = GuessGame(target_number='5678')
+        result = game.check_guess('1234')
         self.assertEqual(result, ("Hints: circle, , , ", 1))
     # new usecase begins
     def test_continuous_guessing(self):
